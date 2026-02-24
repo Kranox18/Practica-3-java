0d575761878333b710c882ccfa131ddfed80554c
BuildTools <unconfigured@null.spigotmc.org>
2026-02-24 23:11:12 +0100

    tercer commit, he hecho algo de la clase de Restaurante. En  general creo que me falta nivel para esta practica.

--- src/Lab3/Restaurante.java
+++ src/Lab3/Restaurante.java
@@ -1,4 +1,52 @@
 package Lab3;
 
 public class Restaurante extends Comercio{
+    private String[] menuDiario;
+    private int numMesas;
+    private int capacidad;
+
+    public Restaurante(){}
+
+    public Restaurante(String[] menuDiario, int numMesas, int capacidad) {
+
+
+        this.menuDiario = menuDiario;
+        this.numMesas = numMesas;
+        this.capacidad = capacidad;
+    }
+
+    public String[] getMenuDiario() {
+        return menuDiario;
+    }
+
+    public void setMenuDiario(String[] menuDiario) {
+        this.menuDiario = menuDiario;
+    }
+
+    public int getNumMesas() {
+        return numMesas;
+    }
+
+    public void setNumMesas(int numMesas) {
+        this.numMesas = numMesas;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+
+    //metodos
+
+
+
+
+
+
+
+
 }
--- src/Lab3/VehiculoParaReparar.java
+++ src/Lab3/VehiculoParaReparar.java
@@ -6,10 +6,11 @@ public class VehiculoParaReparar extends Vehiculo{
     private boolean reparado;
     private int prioridad;
 
-    public VehiculoParaReparar(String marca, String modelo, String matricula, String averia, int prioridad) {
+    public VehiculoParaReparar(String marca, String modelo, String matricula, String averia,boolean reparado, int prioridad) {
         super(marca, modelo, matricula);
         this.averia = averia;
         this.prioridad = prioridad;
+        this.reparado = false;
     }
 
     public String getAveria() {
@@ -38,11 +39,11 @@ public class VehiculoParaReparar extends Vehiculo{
 
     //gemini
     @Override
-    public VehiculoVenta clone() {
+    public VehiculoParaReparar clone() {
         try {
             // Llamamos al clone de la superclase (Vehiculo)
             // que a su vez llamó a Object.clone()
-            return (VehiculoVenta) super.clone();
+            return (VehiculoParaReparar) super.clone();
         } catch (CloneNotSupportedException e) {
             // Esto no debería pasar si implementamos Cloneable
             return null;
